8a17cf11a7ddc6a0bf2ac2461c7155b935a3992a
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-09 14:22:48 +0530

    Ready to deploy

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.get('/scrape', (req, res) => {
+    const url = process.env.URL;
+
+    request(url, (error, response, html) => {
+        if (!error) {
+            const $ = cheerio.load(html);
+
+            let projects = [];
+
+            $('.main-column').each(function () {
+                projects.push({
+                    name: $(this).find('.project-name').text(),
+                    description: $(this).find('.description').text(),
+                });
+            });
+
+            res.send({
+                projects: projects,
+            });
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
